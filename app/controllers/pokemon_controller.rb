e76607e1bd6744254b1527d0cfc6c036c449cf7b
Phangster <mynamebryanphang@gmail.com>
2018-07-18 21:10:52 +0800

    Completed

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,31 +1,47 @@
 class PokemonController < ApplicationController
-    def show_details
-      @pokemon = Pokemon.find( Integer(params[:id]) )
-    end
+
+  def index
+    #this instance variable "SELECT "pokemons".* FROM "pokemons"
+    @pokemons = Pokemon.all
+  end
+
+  def show
+    #  SELECT  "pokemons".* FROM "pokemons" WHERE "pokemons"."id" = $1
+    @pokemon = Pokemon.find(params[:id])
+  end
   
-    def create_form
-    end
+  def new
+  #renders new.html.erb in views folder because they have the same name
+  end
+
+  def create
+    @pokemons = Pokemon.new(pokemon_params)
+
+    @pokemons.save
+    redirect_to pokemon_index_path
+  end
+
+  def update
+    @pokemon = Pokemon.find(params[:id])
+    @pokemon.update(pokemon_params)
+    redirect_to pokemon_index_path
+  end 
   
-    def create
-      Pokemon.create(name: params[:pokemon][:name], img: params[:pokemon][:img], height: params[:pokemon][:height], weight: params[:pokemon][:weight], candy: params[:pokemon][:candy], candy_count: params[:pokemon][:candy_count], egg: params[:pokemon][:egg], avg_spawns: params[:pokemon][:avg_spawns], spawn_time: params[:pokemon][:spawn_time])
-      new_pokemon = Pokemon.find_by( name: params[:pokemon][:name] )
-      new_pokemon.update_attribute(:num, new_pokemon.id.to_s)
-      redirect_to action: "show_details", id: new_pokemon.id
-    end
-  
-    def update_form
-      @pokemon = Pokemon.find(Integer(params[:id]))
-    end
-  
-    def update
-      pokemon = Pokemon.find(Integer(params[:id]))
-      pokemon.update(name: params[:pokemon][:name], img: params[:pokemon][:img], height: params[:pokemon][:height], weight: params[:pokemon][:weight], candy: params[:pokemon][:candy], candy_count: params[:pokemon][:candy_count], egg: params[:pokemon][:egg], avg_spawns: params[:pokemon][:avg_spawns], spawn_time: params[:pokemon][:spawn_time])
-      redirect_to action: "show_details", id: params[:id]
-    end
-  
-    def delete
-      pokemon = Pokemon.find(Integer(params[:id]))
-      pokemon.destroy
-      redirect_to "/"
-    end
+  def edit
+    @pokemon = Pokemon.find(params[:id])
+  end
+
+  def destroy
+    @pokemon = Pokemon.find(params[:id])
+
+    @pokemon.destroy
+    redirect_to pokemon_index_path
+  end
+
+  private
+
+  def pokemon_params
+    params.require(:pokemon).permit(:num, :name, :img, :height, :weight, :candy, :candy_count, :egg, :avg_spawns, :spawn_time)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+# ------ created a model route that points to the database --------#
+
+# rails g model pokemons num:integer name:string img:string height:string weight:string candy:string candy_count:string egg:string avg_spawns:string spawn_time:string
+
+# ------ create and migrate --------#
+# `rails db:create`
+# `rails db:migrate`
+# `rails db:seed`
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -7,6 +16,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 #clear the table before we seed
+
 Pokemon.destroy_all
 
 json = ActiveSupport::JSON.decode(File.read('db/pokedex.json'))
